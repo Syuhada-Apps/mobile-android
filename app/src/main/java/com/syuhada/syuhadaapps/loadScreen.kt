a880e5e40631a36286ede41c33aea9b78b2b88f8
Eko Nur Cahyo <62656387+konucha@users.noreply.github.com>
2023-01-19 17:53:54 +0700

    add the logo xml and rename the mainactivity to loadscreen

--- app/src/main/java/com/syuhada/syuhadaapps/loadScreen.kt
+++ app/src/main/java/com/syuhada/syuhadaapps/loadScreen.kt
@@ -3,9 +3,9 @@ package com.syuhada.syuhadaapps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class loadScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.load_screen)
     }
 }
